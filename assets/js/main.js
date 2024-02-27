26348333aa930720a907658a429acdbb25b528c5
mattemutti <mattemutti@gmail.com>
2024-02-27 16:03:29 +0100

    update correction function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,54 @@
 // EX 1 - PALINDROMO	
 
-
 // - chiedere con un prompt all'utente di inserire una parola e salvarla in una variabile
-const wordUser = "anna"; //prompt("inserisci una parola")
+let wordUser = prompt("inserisci una parola"); // dichiaro la variabile che ha lo stesso nome della variabile del parametro della funzione.
 console.log(wordUser);
-
-// trasformo una stringa in un array dividendo i vari array con "" 
-//(niente , quindi ogni parola diventa un oggetto dell'array)
-const wordArray = wordUser.split("");
-console.log(wordArray);
-
-//iverte l'ordine delle posizioni dell'array dall'ultima posizione alla prima
-const wordRevArray = wordArray.reverse();
-console.log(wordRevArray);
-
-// trasformare l'array in stringa per avere la parola al contrario con i separatore "" 
-//quindi attacca tutte le lettere in un unica stringa
-// - salvare la parola al contrario in un altr variabile e metterle a confronto con un if
-const wordRev = wordRevArray.join("");
-console.log(wordRev);
-
-
 
 // - devo controllare se girando la parola, è uguale alla parola originale 
 
-
 function controlWordReverse(wordUser) {
 
-	if (wordUser === wordRev){
-		return (`La parola ${wordUser} è palindroma`)
-				
+	const wordShow = wordUser
+
+	
+	wordUser = wordUser.toLowerCase();
+	console.log(wordUser);
+	
+	let wordOriginalUser = wordUser
+
+	// trasformo una stringa in un array dividendo i vari array con "" 
+	//(niente , quindi ogni parola diventa un oggetto dell'array)
+	wordUser = wordUser.split("");
+	console.log(wordUser);
+
+	//iverte l'ordine delle posizioni dell'array dall'ultima posizione alla prima
+	wordUser = wordUser.reverse();
+	console.log(wordUser);
+
+	// trasformare l'array in stringa per avere la parola al contrario con i separatore "" 
+	//quindi attacca tutte le lettere in un unica stringa
+	// - salvare la parola al contrario in un altr variabile e metterle a confronto con un if
+	const wordRev = wordUser.join("");
+	console.log(wordRev);
+
+	// - dare la risposta
+
+	if (wordOriginalUser === wordRev) {
+		return alert(`La parola ${wordShow} è palindroma`);
+
 	}
 
-	return (`La parola ${wordUser} non è palindroma`)
+	return alert(`La parola ${wordShow} non è palindroma`);
 
 }
 
-
-console.log(controlWordReverse(wordUser));
-
+console.log(controlWordReverse(wordUser));	// richiamiamo la funzione per controllare la parola.
 
 
 
 
 
-// - dare la risposta
+
 
 
 
