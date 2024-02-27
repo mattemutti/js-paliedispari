092f642d2aba6eb196ac54aa5932dab796ca669b
mattemutti <mattemutti@gmail.com>
2024-02-27 16:31:50 +0100

    add user choice

--- Ex_2/assets/js/main.js
+++ Ex_2/assets/js/main.js
@@ -2,11 +2,28 @@
 
 // - far scegliere all'utente pari o disapri con un prompt
 
-const userChoice = prompt
+let userChoice = prompt("Se scegli pari, scrivilo qui sotto, altrimenti otterrai dispari:") //Selezione da parte dell'utente di pari o dispari
+console.log(userChoice);
 
-// - far inserire un numero da 1 a 5
+userChoice = userChoice.toLowerCase() // trasformo il testo in minuscolo per compararlo
+
+if (userChoice == "pari"){
+	userChoice = 0
+} else{
+	userChoice = 1
+}
+console.log(userChoice);
+
+// - far inserire un numero da 1 a 5 con un Prompt
+
+const numberUser = prompt('Inserisci un numero da 1 a 5:')
+console.log(numberUser);
+
+if (numberUser )
 
 // - generare un numero randomico da 1 a 5 dalla funzione Math
+
+const numPc = Math.round(Math.random()*5)+1;
 
 // - controllare se il numero è pari o dispari
 
