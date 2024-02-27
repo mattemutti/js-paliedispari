4f116fe6c0c711e01d519db3f95c480c0002a258
mattemutti <mattemutti@gmail.com>
2024-02-27 15:34:13 +0100

    add control function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,48 @@
 
 
 // - chiedere con un prompt all'utente di inserire una parola e salvarla in una variabile
-const wordUser = "fragile"; //prompt("inserisci una parola")
+const wordUser = "anna"; //prompt("inserisci una parola")
 console.log(wordUser);
 
 // trasformo una stringa in un array dividendo i vari array con "" 
 //(niente , quindi ogni parola diventa un oggetto dell'array)
-const wordArray = wordUser.split("") 
+const wordArray = wordUser.split("");
 console.log(wordArray);
 
 //iverte l'ordine delle posizioni dell'array dall'ultima posizione alla prima
-const wordRev = wordArray.reverse() 
-console.log(wordArray);
+const wordRevArray = wordArray.reverse();
+console.log(wordRevArray);
 
-// trasformare l'array in stringa per avere la parola al contrario 
-
-
-
-// - anzi devo controllare se gurando la parola, è uguale alla parola originale 
+// trasformare l'array in stringa per avere la parola al contrario con i separatore "" 
+//quindi attacca tutte le lettere in un unica stringa
 // - salvare la parola al contrario in un altr variabile e metterle a confronto con un if
+const wordRev = wordRevArray.join("");
+console.log(wordRev);
+
+
+
+// - devo controllare se girando la parola, è uguale alla parola originale 
+
+
+function controlWordReverse(wordUser) {
+
+	if (wordUser === wordRev){
+		return (`La parola ${wordUser} è palindroma`)
+				
+	}
+
+	return (`La parola ${wordUser} non è palindroma`)
+
+}
+
+
+console.log(controlWordReverse(wordUser));
+
+
+
+
+
+
 // - dare la risposta
 
 
