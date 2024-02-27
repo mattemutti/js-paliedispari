cfd6e2ee658992cd9c91fc5e5e9bfe920c41fb3d
mattemutti <mattemutti@gmail.com>
2024-02-27 16:14:53 +0100

    add folder ex 2

--- Ex_1/assets/js/main.js
+++ Ex_1/assets/js/main.js
@@ -58,25 +58,3 @@ console.log(controlWordReverse(wordUser));	// richiamiamo la funzione per contro
 
 
  
-
-
-
-
-
-
-
-
-
-// EX 2 - PARI O DISPARI
-
-// - far scegliere all'utente pari o disapri con un prompt
-
-const userChoice = prompt
-
-// - far inserire un numero da 1 a 5
-
-// - generare un numero randomico da 1 a 5 dalla funzione Math
-
-// - controllare se il numero è pari o dispari
-
-// - dichiarare chio ha vinto
---
+++ Ex_2/assets/js/main.js
@@ -0,0 +1,13 @@
+// EX 2 - PARI O DISPARI
+
+// - far scegliere all'utente pari o disapri con un prompt
+
+const userChoice = prompt
+
+// - far inserire un numero da 1 a 5
+
+// - generare un numero randomico da 1 a 5 dalla funzione Math
+
+// - controllare se il numero è pari o dispari
+
+// - dichiarare chio ha vinto
